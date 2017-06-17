2be2a06956d32538bee32ec72694c3c584bb64f0
Prince Akpa <Prince Akpa@PRINCE-PC.(none)')>
2017-06-17 02:51:47 +0100

    adding repo

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
+
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+    $('#navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+
+
+	
+	//Menu Background Change
+	
+	var windowWidth = $(window).width();
+    if (windowWidth > 757) {
+
+        
+          
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 500) {
+                    $('.navbar').fadeIn(500);
+                    $('.navbar').addClass('menu-bg');
+
+                } else {
+                    
+                    $('.navbar').removeClass('menu-bg');
+                }
+            });
+        
+    }
+	$('#bs-example-navbar-collapse-1').localScroll();
+	
+	$(window).scroll(function(){
+		if($(this).scrollTop()>200)
+		{
+			$('.scrollup').fadeIn();
+		}
+		else
+		{
+			$('.scrollup').fadeOut();
+		}		
+	});
+	
+
+	
+});
+
+
---
+++ firstform.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	  <form action="POST" method="team.html">
+            <p>Firstname: <input type="text" name="firstname" value="firstname"></p>
+        </form>
+
+</body>
+</html>
